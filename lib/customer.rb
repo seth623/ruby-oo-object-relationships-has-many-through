b5a3882727b72a72e8c32aae0c723ee94463cf13
seth <seth623@gmail.com>
2022-02-23 20:16:15 -0700

    Completed Assignment

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
+
+    @@all = []
+    attr_accessor :name, :age 
   
+    def initialize(name, age)
+        @name = name 
+        @age = age
+        @@all << self  
+    end
+    
+    def meals
+        Meal.all.select{ |meal| meal.customer == self }
+    end 
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+    
+    def waiters
+        self.meals.map{ |meal| meal.waiter }
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,34 @@
 class Waiter
 
+    @@all = []
+    attr_accessor :name, :years_of_experience
+
+    def initialize(name, years_of_experience)
+        @name = name 
+        @years_of_experience = years_of_experience
+        @@all << self 
+    end
+
+    def best_tipper
+        
+        best_tipped_meal = self.meals.max do |meal_a, meal_b|
+            meal_a.tip <=> meal_b.tip  
+        end
+        
+        best_tipped_meal.customer 
+
+    end 
+
+    def meals 
+        Meal.all.select{ |meal| meal.waiter == self }
+    end 
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
 end
